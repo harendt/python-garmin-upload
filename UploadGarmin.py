e7679fbe4876dae728bf4541ecabba27cc6e3089
Bastian Harendt <b.harendt@gmail.com>
2013-11-26 00:50:42 +0100

    Added a (so far missing) explicit version of the BSD license and updated the copyright notice

--- UploadGarmin.py
+++ UploadGarmin.py
@@ -3,17 +3,36 @@ Upload Garmin
 
 Handle the operation to upload to the Garmin Connect Website.
 
-This file is derived from orignal work by Chmouel Boudjnah <chmouel@chmouel.com>
-Original Source: https://github.com/chmouel/python-garmin-upload
+Copyright (c) 2010, Chmouel Boudjnah <chmouel@chmouel.com>
+Copyright (c) 2012, David Lotton <yellow56@gmail.com>
+Copyright (c) 2013, Bastian Harendt <b.harendt@gmail.com>
 
-Modifications Copyright (c) David Lotton 01/2012
+All rights reserved.
 
-License: BSD
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-Modified 01/2012 by David Lotton to allow all upload file formats (tcx, fit, gpx)
-supported by connect.garmin.com.  Renamed all incorrectly named references to
-'ctx' as 'tcx'. Corrected URI in original 'upload_tcx' function, adding the file
-type extension to the URI.
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. The names of the copyright holders and contributors may not be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import urllib2

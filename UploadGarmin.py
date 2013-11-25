a6defd9ff0a6623fb76231e97127b302e0590024
Bastian Harendt <b.harendt@gmail.com>
2013-11-25 19:19:26 +0100

    Imported modifications by David Lotton (http://sourceforge.net/projects/gcpuploader/)

--- UploadGarmin.py
+++ UploadGarmin.py
@@ -4,10 +4,29 @@ Upload Garmin
 Handle the operation to upload to the Garmin Connect Website.
 
 """
+# 
+# This file is derived from orignal work by Chmouel Boudjnah <chmouel@chmouel.com>
+# Original Source: https://github.com/chmouel/python-garmin-upload
+#
+# Modifications Copyright (c) David Lotton 01/2012
+#
+# License: BSD
+#
+# Modified 01/2012 by David Lotton to allow all upload file formats (tcx, fit, gpx)
+# supported by connect.garmin.com.  Renamed all incorrectly named references to
+# 'ctx' as 'tcx'. Corrected URI in original 'upload_tcx' function, adding the file
+# type extension to the URI.
+#
+
 import urllib2
 import urllib
 import MultipartPostHandler
-import simplejson
+try:
+    import simplejson
+except ImportError:
+    import json as simplejson
+import os.path
+
 
 class UploadGarmin:
     """
@@ -44,26 +63,89 @@ class UploadGarmin:
         vary_header = status.headers['vary']
         status.close()
 
-        #Hacky but that's the only way I can know if succeed or not
-        if 'X-Secure' in vary_header:
-            raise Exception("Invalid Login or Password")
+        #dlotton - If login succeeds this returns a JSON with the username
+        # something like {"username":"fred"}
+        json = self.opener.open('http://connect.garmin.com/user/username').read().strip()
 
+        # Uncomment for debugging
+        #print '\tLogin_Username: ' + username
+        #print '\tJSON: ' + json
+        #print '\tJSON_Username: ' + simplejson.loads(json)['username']
 
-    def upload_ctx(self, ctx_file):
+        if simplejson.loads(json)['username'] != '':
+            return(True)
+        else:
+            return(False)
+    
+
+    def upload_file(self, uploadFile):
         """
-        Upload a CTX File
+        Upload a File
 
         You need to be logged already
 
-        @ivar ctx_file: The CTX file name to upload
-        @type ctx_file: str
+        @ivar uploadFile: The TCX, GPX, or FIT file name to upload
+        @type uploadFile: str
+        """
+
+        extension = os.path.splitext(uploadFile)[1].lower()
+
+        # Valid File extensions are .tcx, .fit, and .gpx
+        if extension not in ['.tcx', '.fit', '.gpx']:
+            raise Exception("Invalid File Extension")
+
+        if extension == '.fit':
+            mode = 'rb'
+        else:
+            mode = 'r'
+        
+        params = {
+            "responseContentType" : "text%2Fhtml",
+            "data" : open(uploadFile, mode)
+        }
+
+        uploadUri='http://connect.garmin.com/proxy/upload-service-1.1/json/upload/'+extension
+        # debug:
+        #print '\tURI: ' + uploadUri
+
+        output = self.opener.open(uploadUri, params)
+        if output.code != 200:
+            raise Exception("Error while uploading")
+        json = output.read()
+        output.close()
+
+        # debug: Helpful info - uncomment the following print statements
+        #print '--------------debug-------------'
+        #print uploadFile
+        #print json
+        #print '--------------debug-------------'
+        if len(simplejson.loads(json)["detailedImportResult"]["successes"])==0:
+            if len(simplejson.loads(json)["detailedImportResult"]["failures"]) != 0:
+                # File already exists on Garmin Connect
+                return ['EXISTS', simplejson.loads(json)["detailedImportResult"]["failures"][0]["internalId"]]
+            else:
+                # Don't know what failed
+                return ['FAIL', 'Upload Fail']
+        else:
+            # Upload was successsful
+            return ['SUCCESS', simplejson.loads(json)["detailedImportResult"]["successes"][0]["internalId"]]
+
+    def upload_tcx(self, tcx_file):
+        """
+        Upload a TCX File
+
+        You need to be logged already
+
+        @ivar tcx_file: The TCX file name to upload
+        @type tcx_file: str
         """
         params = {
             "responseContentType" : "text%2Fhtml",
-            "data" : open(ctx_file)
+            "data" : open(tcx_file)
         }
         
-        output = self.opener.open('http://connect.garmin.com/proxy/upload-service-1.1/json/upload', params)
+        #dlotton - added '.tcx' to the end of the URI below
+        output = self.opener.open('http://connect.garmin.com/proxy/upload-service-1.1/json/upload/.tcx', params)
         if output.code != 200:
             raise Exception("Error while uploading")
         json = output.read()
@@ -104,5 +186,6 @@ class UploadGarmin:
 if __name__ == '__main__':
     g = UploadGarmin()
     g.login("username", "password")
-    wId = g.upload_ctx('/tmp/a.ctx')
+    wId = g.upload_tcx('/tmp/a.tcx')
+    wInfo = g.upload_file('/tmp/a.tcx')
     g.name_workout(wId, "TestWorkout")

76971411b266c836cb37979b466cb0faf7f95ed6
Bastian Harendt <b.harendt@gmail.com>
2013-11-27 22:58:13 +0100

    Refactoring of upload method

--- UploadGarmin.py
+++ UploadGarmin.py
@@ -47,6 +47,7 @@ import os.path
 
 BaseUrl = 'https://connect.garmin.com/'
 UserService = BaseUrl + 'proxy/user-service-1.0/json/'
+UploadService = BaseUrl + 'proxy/upload-service-1.1/json/'
 
 """
 Upload Garmin
@@ -56,7 +57,7 @@ Handle operation to open to Garmin
 class UploadGarmin:
 
 	userId = -1
-	userName = ""
+	userName = ''
 
 	def __init__(self):
 		# see: http://docs.python.org/2/library/urllib2.html#urllib2.build_opener
@@ -75,19 +76,19 @@ class UploadGarmin:
 	@type password: str
 	"""
 	def signIn(self, user, password):
-		try:
-			# it seems that all of the following parameters are required for a
-			# successful login
-			params = {
-					'login' : 'login',
-					'login:loginUsernameField' : user,
-					'login:password' : password,
-					'login:signInButton' : 'Sign In',
-					'javax.faces.ViewState' : 'j_id1'
-			}
-			params = urllib.urlencode(params)
+		# it seems that all of the following parameters are required for a
+		# successful login
+		params = {
+				'login' : 'login',
+				'login:loginUsernameField' : user,
+				'login:password' : password,
+				'login:signInButton' : 'Sign In',
+				'javax.faces.ViewState' : 'j_id1'
+		}
+		params = urllib.urlencode(params)
 
-			# open the sign in page once
+		try:
+			# open the sign in page once (required for a successful login)
 			self.opener.open(BaseUrl + 'signin').read()
 
 			# send login parameters
@@ -99,20 +100,27 @@ class UploadGarmin:
 			output = self.opener.open(BaseUrl + 'user/username')
 			output = json.loads(output.read())
 
+			# get account information (username and user ID)
 			if output['username'] != '':
 				output = self.opener.open(UserService + 'account')
 				output = json.loads(output.read())
 				self.userId = int(output['account']['userId'])
 				self.userName = output['account']['username']
-				print 'Signed in as user "%s" (ID: %d)' % (self.userName, self.userId)
-				return True
 			else:
-				print 'Sign in as user "%s" failed' % user
-				return False
+				self.userId = -1
+				self.userName = ''
 
-		except BaseException as e:
+		except Exception as e:
 			print 'Sign in failed unexpectedly: %s' % str(e)
 			return False
+
+		if self.userId > 0:
+			print 'Signed in as user "%s" (ID: %d)' % (self.userName, self.userId)
+			return True
+		else:
+			print 'Sign in as user "%s" failed' % user
+			return False
+
 
 	"""
 	Upload a File
@@ -122,49 +130,53 @@ class UploadGarmin:
 	@ivar uploadFile: The TCX, GPX, or FIT file name to upload
 	@type uploadFile: str
 	"""
-	def upload_file(self, uploadFile):
+	def uploadFile(self, filename):
+		# Split the filename into root and extension
+		# see: http://docs.python.org/2/library/os.path.html#os.path.splitext
+		fileExtension = os.path.splitext(filename)[1].lower()
 
-		extension = os.path.splitext(uploadFile)[1].lower()
+		# Valid file extensions are .tcx, .fit, and .gpx
+		if fileExtension not in ['.tcx', '.fit', '.gpx']:
+			raise Exception('Invalid file extension "%s"' % fileExtension)
 
-		# Valid File extensions are .tcx, .fit, and .gpx
-		if extension not in ['.tcx', '.fit', '.gpx']:
-			raise Exception("Invalid File Extension")
-
-		if extension == '.fit':
-			mode = 'rb'
+		if fileExtension == '.fit':
+			openMode = 'rb'
 		else:
-			mode = 'r'
+			openMode = 'r'
 
 		params = {
-			"responseContentType" : "text%2Fhtml",
-			"data" : open(uploadFile, mode)
+			"data" : open(filename, openMode)
 		}
 
-		uploadUri='http://connect.garmin.com/proxy/upload-service-1.1/json/upload/'+extension
-		# debug:
-		#print '\tURI: ' + uploadUri
+		print 'Uploading file "%s"' % filename
+		try:
+			output = self.opener.open(UploadService + 'upload/' + fileExtension, params)
+			output = simplejson.loads(output.read())
+			successes = output['detailedImportResult']['successes']
+			failures  = output['detailedImportResult']['failures']
+		except Exception as e:
+			print 'Upload failed unexpectedly: %s' % str(e)
+			return False, -1
 
-		output = self.opener.open(uploadUri, params)
-		if output.code != 200:
-			raise Exception("Error while uploading")
-		json = output.read()
-		output.close()
+		if len(successes) > 0 and 'internalId' in successes[0]:
+			activityId = int(successes[0]['internalId'])
+			print 'Upload succeeded (activity ID: %d)' % activityId
+			return True, activityId
 
-		# debug: Helpful info - uncomment the following print statements
-		#print '--------------debug-------------'
-		#print uploadFile
-		#print json
-		#print '--------------debug-------------'
-		if len(simplejson.loads(json)["detailedImportResult"]["successes"])==0:
-			if len(simplejson.loads(json)["detailedImportResult"]["failures"]) != 0:
-				# File already exists on Garmin Connect
-				return ['EXISTS', simplejson.loads(json)["detailedImportResult"]["failures"][0]["internalId"]]
-			else:
-				# Don't know what failed
-				return ['FAIL', 'Upload Fail']
-		else:
-			# Upload was successsful
-			return ['SUCCESS', simplejson.loads(json)["detailedImportResult"]["successes"][0]["internalId"]]
+		if len(failures) > 0 and 'internalId' in failures[0]:
+			activityId = int(failures[0]['internalId'])
+			# get failure code (code #202 stands for 'duplicate activity')
+			try:
+				errorCode = int(failures[0]['messages'][0]['code'])
+			except Exception:
+				errorCode = -1
+			if errorCode == 202:
+				print 'Activity already exists (activity ID: %d)' % activityId
+				return False, activityId
+
+		print 'Upload failed unexpectedly'
+		return False, -1
+
 
 	def upload_tcx(self, tcx_file):
 		"""
